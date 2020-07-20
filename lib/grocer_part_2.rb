b7576bf7310e5829eed3506d15fcc83b6e84b06e
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-20 23:57:58 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -37,7 +37,7 @@ end
 def apply_clearance(cart)
   cart.each do |item|
     if(item[:clearance] == true)
-      item[:price] = (item[:price] * 8).round(2)
+      item[:price] = (item[:price] * .8).round(2)
     end 
   end 
 
