a26fae9bc4591f85c24addf25bad8a05ba219ca5
Juan Carlos Patron <charliepatronr@gmail.com>
2020-07-21 00:14:02 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -54,16 +54,7 @@ def checkout(cart, coupons)
   end 
   if(tot > 100)
     tot = (tot * 0.9).round(2)
-  end 
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  end
   tot
 end
 
